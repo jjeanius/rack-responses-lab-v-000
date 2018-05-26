f45e47dfa26f51877f0509db882cac322fbbb63a
Judy McGoldrick <jjeanius@verizon.net>
2018-05-26 02:40:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,9 @@ class Application
     resp = Rack::Response.new
     now = Time.now
     if now.hour < 12
-      resp write "Good Morning!"
+      resp.write "Good Morning!"
     if now.hour >12
-      resp write "Good Afternoon!"
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
